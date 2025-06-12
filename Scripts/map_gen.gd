b2bd90043e0fa658a56187be13f936f061079d5b
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-06-11 19:49:19 -0700

    Migrated to TileMapLayer
    Visualization using 2d tiles, also fixed zoom for the umpteenth time

--- Scripts/map_gen.gd
+++ Scripts/map_gen.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export_range(1, 10) var sigma : float = 3.5 # smaller sigma means bigger island
+@export var map_tile : TileMapLayer
+@export var resource_tile : TileMapLayer
 var map : Array[Array] = []
 
 func _ready():
@@ -12,16 +14,21 @@ func _draw():
 	map_visualize()
 
 func gen_map(width : int, height : int) -> void:
-	var scale = Vector2(width, height)
+	var scale_vec = Vector2(width, height)
 	var noise = _gen_noise(width, height)
 	for x in range(width):
 		map.append([])
 		for y in range(height):
 			var tile_type = Global.TILE_TYPE.SEA
-			var dist = (Vector2(x,y)/scale).distance_to(Vector2(.5,.5))
+			var tile_id: Vector2i = Global.TILE_ID["SEA"]
+			var dist = (Vector2(x,y)/scale_vec).distance_to(Vector2(.5,.5))
 			if _gaussian(dist, sigma) * (1 + noise.get_noise_2d(x, y)/4) > 0.4:
 				#TODO Scale sigma and noise according to scale
 				tile_type = Global.TILE_TYPE.LAND
+				tile_id = Global.TILE_ID["LAND"]
+				if randf() > .3:
+					tile_type = Global.TILE_TYPE.RESOURCE
+					resource_tile.set_cell(Vector2i(x-width/2,y-height/2), 0, Global.TILE_ID["RESOURCE"])
 			
 			#var tile_type = Global.TILE_TYPE.LAND
 			#if x == 0 or x == width - 1 or y == 0 or y == height - 1:
@@ -29,6 +36,7 @@ func gen_map(width : int, height : int) -> void:
 			#elif randf() < 0.1:
 				#tile_type = Global.TILE_TYPE.RESOURCE
 			map[x].append(tile_type)
+			map_tile.set_cell(Vector2i(x-width/2,y-height/2), 0, tile_id)
 
 func _gaussian(dist: float, sig: float) -> float:
 	return exp(-pow(dist, 2) / 2 * pow(sig, 2))
@@ -40,7 +48,7 @@ func _gen_noise(width: float, height: float) -> FastNoiseLite:
 	noise.fractal_type = FastNoiseLite.FRACTAL_NONE
 	noise.fractal_octaves = 3
 	noise.frequency = 0.08
-	#noise.offset = Vector3(width/2, height/2, 0)
+	noise.offset = Vector3(width/2, height/2, 0)
 	#noise.fractal_gain
 	#noise.fractal_lacunarity
 	return noise
--- hmmm/Scripts/global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-enum TILE_TYPE {
-	SEA,
-	LAND,
-	RESOURCE,
-}
-
-@onready var screen_size : Vector2 = get_viewport().get_visible_rect().size
